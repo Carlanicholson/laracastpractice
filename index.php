a409184c8ae1c24dcd13d6c3abb2610f5264be15
Carla Nicholson <carla@codepotato.co.uk>
2022-02-28 14:01:03 +0000

    class added

--- index.php
+++ index.php
@@ -18,5 +18,37 @@ require 'index.view.php';
 //dd ('hello world');
 
 
+class Team {
+    protected $name;
+
+    protected $members = [];
+
+    public function _construct($name)
+
+{
+    $this->name = $name;
+}
+
+public function name()
+{
+ return $this->name;
+}
+
+public function members()
+{
+    return $this->members;
+}
+
+public function add($name)
+{
+    $this->members[] = $name;
+}
+}
 
 
+$acne = new Team('Acne');
+
+$acne->add('John Doe');
+$acne->add('Jane Doe');
+
+var_dump($acne->members());
--- index.view.php
+++ index.view.php
@@ -29,10 +29,13 @@
 <!--</ul>-->
 
 <?php
+//
+//checkAge($age = 33 );
+//
+//?>
 
-checkAge($age = 33 );
 
-?>
+
 </body>
 
 </html>
