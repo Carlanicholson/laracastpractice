d6fa665f0c8c3bfccf22218a54e23221b08a0875
Carla Nicholson <carla@codepotato.co.uk>
2022-03-03 14:35:30 +0000

    added functions

--- index.php
+++ index.php
@@ -58,31 +58,101 @@ require 'index.view.php';
 //
 //var_dump($acne->members());
 
+//inheritance
 
-class CoffeeMaker
+//class CoffeeMaker
+//{
+//    public function brew()
+//    {
+//        var_dump('Brewing the coffee');
+//    }
+//
+//}
+////"is a"
+//class SpecialityCoffeeMaker extends CoffeeMaker
+//{
+//    public function brewLatte()
+//    {
+//        var_dump('Brewing a latte');
+//    }
+//}
+//
+//class ExtraCoffeeMaker extends SpecialityCoffeeMaker
+//
+//{
+//    public function brewFlatWhite()
+//    {
+//        var_dump('Brewing a Flat White');
+//    }
+//}
+//
+//(new ExtraCoffeeMaker())->brewFlatWhite();
+//
+//abstract class AchievementType
+//{
+//    public function name()
+//    {
+//        $class  = (new ReflectionClass($this))->getShortName();
+//        // FirstThousandPoint => First Thousand Points
+//        return trim(preg_replace('/[A-Z]/', ' $0', $class));
+//    }
+//
+//    public function icon()
+//    {
+//        return strtolower(str_replace(' ', '-', $this->name())).' .png';
+//    }
+//
+//    abstract public function qualifier($user);
+//
+//}
+//
+//class FirstThousandPoints extends  AchievementType
+//{
+//    public function qualifier($user)
+//{
+//    //
+//}
+//}
+//
+//class FirstBestAnswer extends AchievementType
+//{
+//    public function qualifier($user)
+//    {
+//    }
+//
+//}
+//
+//$achievement  = new FirstBestAnswer();
+//
+
+
+
+
+class Age
 {
-    public function brew()
+    private $age;
+
+    public function __construct($age)
     {
-        var_dump('Brewing the coffee');
+        if ($age < 0 || $age > 120){
+            throw new InvalidArgumentException('That makes no sense');
+        }
+        $this->age = $age;
+    }
+    public function increment()
+
+    {
+        $this->age+=1;
+    }
+
+    public function get()
+    {
+        return $this->age;
     }
 
 }
-//"is a"
-class SpecialityCoffeeMaker extends CoffeeMaker
-{
-    public function brewLatte()
-    {
-        var_dump('Brewing a latte');
-    }
-}
 
-class ExtraCoffeeMaker extends SpecialityCoffeeMaker
+$age = new Age(35);
+$age->increment();
 
-{
-    public function brewFlatWhite()
-    {
-        var_dump('Brewing a Flat White');
-    }
-}
-
-(new ExtraCoffeeMaker())->brewFlatWhite();
+echo ($age->get());
